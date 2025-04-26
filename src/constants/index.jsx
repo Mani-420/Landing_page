58fdbc7e831b0f559d82d2ad2d8a0d83b1cf06b2
Mani-420 <tahirabdulrehman46@gmail.com>
2025-04-26 23:25:29 +0500

    Object Literals and Arrays

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -16,7 +16,7 @@ export const navItems = [
   { label: 'Services', href: '#' },
   { label: 'Workflow', href: '#' },
   { label: 'Pricing', href: '#' },
-  { label: 'Testimonials', href: '#' },
+  { label: 'Testimonials', href: '#' }
 ];
 
 export const testimonials = [
@@ -24,38 +24,38 @@ export const testimonials = [
     user: 'John Doe',
     company: 'Stellar Solutions',
     image: user1,
-    text: 'I am extremely satisfied with the services provided. The team was responsive, professional, and delivered results beyond my expectations.',
+    text: 'I am extremely satisfied with the services provided. The team was responsive, professional, and delivered results beyond my expectations.'
   },
   {
     user: 'Jane Smith',
     company: 'Blue Horizon Technologies',
     image: user2,
-    text: "I couldn't be happier with the outcome of our project. The team's creativity and problem-solving skills were instrumental in bringing our vision to life",
+    text: "I couldn't be happier with the outcome of our project. The team's creativity and problem-solving skills were instrumental in bringing our vision to life"
   },
   {
     user: 'David Johnson',
     company: 'Quantum Innovations',
     image: user3,
-    text: 'Working with this company was a pleasure. Their attention to detail and commitment to excellence are commendable. I would highly recommend them to anyone looking for top-notch service.',
+    text: 'Working with this company was a pleasure. Their attention to detail and commitment to excellence are commendable. I would highly recommend them to anyone looking for top-notch service.'
   },
   {
     user: 'Ronee Brown',
     company: 'Fusion Dynamics',
     image: user4,
-    text: 'Working with the team at XYZ Company was a game-changer for our project. Their attention to detail and innovative solutions helped us achieve our goals faster than we thought possible. We are grateful for their expertise and professionalism!',
+    text: 'Working with the team at XYZ Company was a game-changer for our project. Their attention to detail and innovative solutions helped us achieve our goals faster than we thought possible. We are grateful for their expertise and professionalism!'
   },
   {
     user: 'Michael Wilson',
     company: 'Visionary Creations',
     image: user5,
-    text: 'I am amazed by the level of professionalism and dedication shown by the team. They were able to exceed our expectations and deliver outstanding results.',
+    text: 'I am amazed by the level of professionalism and dedication shown by the team. They were able to exceed our expectations and deliver outstanding results.'
   },
   {
     user: 'Emily Davis',
     company: 'Synergy Systems',
     image: user6,
-    text: 'The team went above and beyond to ensure our project was a success. Their expertise and dedication are unmatched. I look forward to working with them again in the future.',
-  },
+    text: 'The team went above and beyond to ensure our project was a success. Their expertise and dedication are unmatched. I look forward to working with them again in the future.'
+  }
 ];
 
 export const services = [
@@ -63,61 +63,61 @@ export const services = [
     icon: <AppWindowIcon />,
     text: 'Custom Web Development',
     description:
-      'Build scalable, full-stack applications tailored to client needs, from front-end to back-end.',
+      'Build scalable, full-stack applications tailored to client needs, from front-end to back-end.'
   },
   {
     icon: <BotIcon />,
     text: 'Business Process Automation',
     description:
-      'Automate repetitive tasks, workflows, and processes to improve efficiency and reduce costs, saving time and resources.',
+      'Automate repetitive tasks, workflows, and processes to improve efficiency and reduce costs, saving time and resources.'
   },
   {
     icon: <ServerCog />,
     text: 'Website Maintenance and Support',
     description:
-      'Ensure your websites stay updated, fast, and error-free with regular maintenance and support services.',
+      'Ensure your websites stay updated, fast, and error-free with regular maintenance and support services.'
   },
   {
     icon: <WorkflowIcon />,
     text: 'API Development & Integration',
     description:
-      'Create and integrate robust APIs to connect systems seamlessly, enabling smooth data exchange and functionality.',
+      'Create and integrate robust APIs to connect systems seamlessly, enabling smooth data exchange and functionality.'
   },
   {
     icon: <ApertureIcon />,
     text: 'AI & Machine Learning Solutions',
     description:
-      'Build smart systems like predictive models, recommendation engines, and AI chatbots to enhance user experience and decision-making.',
+      'Build smart systems like predictive models, recommendation engines, and AI chatbots to enhance user experience and decision-making.'
   },
   {
     icon: <ShoppingBag />,
     text: 'E-commerce Development',
     description:
-      'Develop secure, customized online stores with full payment and product management solutions.',
-  },
+      'Develop secure, customized online stores with full payment and product management solutions.'
+  }
 ];
 
 export const checklistItems = [
   {
-    title: 'Code merge made easy',
+    title: 'Simplify Code Collaboration',
     description:
-      'Track the performance of your VR apps and gain insights into user behavior.',
+      'Manage, review, and merge code seamlessly across teams with powerful tools designed to boost collaboration.'
   },
   {
-    title: 'Review code without worry',
+    title: 'Seamless Review and Improve Coding',
     description:
-      'Track the performance of your VR apps and gain insights into user behavior.',
+      'SoftTech ensures smooth and efficient code reviews, and error detection.'
   },
   {
-    title: 'AI Assistance to reduce time',
+    title: 'Leverage AI-Powered Assistance',
     description:
-      'Track the performance of your VR apps and gain insights into user behavior.',
+      'Our intelligent AI tools help you write, optimize, and debug code faster.'
   },
   {
-    title: 'Share work in minutes',
+    title: 'Sharing Your Projects Faster',
     description:
-      'Track the performance of your VR apps and gain insights into user behavior.',
-  },
+      'Share updates, new features, or complete projects with your team or clients in just minutes.'
+  }
 ];
 
 export const pricingOptions = [
@@ -128,8 +128,8 @@ export const pricingOptions = [
       'Private board sharing',
       '5 Gb Storage',
       'Web Analytics',
-      'Private Mode',
-    ],
+      'Private Mode'
+    ]
   },
   {
     title: 'Pro',
@@ -138,8 +138,8 @@ export const pricingOptions = [
       'Private board sharing',
       '10 Gb Storage',
       'Web Analytics (Advance)',
-      'Private Mode',
-    ],
+      'Private Mode'
+    ]
   },
   {
     title: 'Enterprise',
@@ -148,9 +148,9 @@ export const pricingOptions = [
       'Private board sharing',
       'Unlimited Storage',
       'High Performance Network',
-      'Private Mode',
-    ],
-  },
+      'Private Mode'
+    ]
+  }
 ];
 
 export const resourcesLinks = [
@@ -158,7 +158,7 @@ export const resourcesLinks = [
   { href: '#', text: 'Documentation' },
   { href: '#', text: 'Tutorials' },
   { href: '#', text: 'API Reference' },
-  { href: '#', text: 'Community Forums' },
+  { href: '#', text: 'Community Forums' }
 ];
 
 export const platformLinks = [
@@ -166,7 +166,7 @@ export const platformLinks = [
   { href: '#', text: 'Supported Devices' },
   { href: '#', text: 'System Requirements' },
   { href: '#', text: 'Downloads' },
-  { href: '#', text: 'Release Notes' },
+  { href: '#', text: 'Release Notes' }
 ];
 
 export const communityLinks = [
@@ -174,5 +174,5 @@ export const communityLinks = [
   { href: '#', text: 'Meetups' },
   { href: '#', text: 'Conferences' },
   { href: '#', text: 'Hackathons' },
-  { href: '#', text: 'Jobs' },
+  { href: '#', text: 'Jobs' }
 ];
