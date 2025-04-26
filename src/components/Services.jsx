5c18756bc02fe924cb9d92855f905d69899154e0
Mani-420 <tahirabdulrehman46@gmail.com>
2025-04-26 23:53:34 +0500

    Services Component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,9 +5,6 @@ const Services = () => {
   return (
     <div className="relative mt-20 border-b border-neutral-800 min-h-[800px]">
       <div className="text-center">
-        <span className="bg-neutral-900 text-blue-500 rounded-full h-6 text-sm font-medium px-2 py-1 uppercase">
-          Services
-        </span>
         <h2 className="text-3xl sm:text-5xl lg:text-6xl mt-10 lg:mt-20 tracking-wide">
           Turn Ideas into Reality <br />
           <span className="bg-gradient-to-r from-blue-500 to-blue-800 text-transparent bg-clip-text">
