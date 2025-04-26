8d5bb276757a2fef99ff764503b6c7bb6ea7e332
Mani-420 <tahirabdulrehman46@gmail.com>
2025-04-26 23:25:57 +0500

    WorkFlow Component

--- src/components/WorkFlow.jsx
+++ src/components/WorkFlow.jsx
@@ -6,9 +6,9 @@ const WorkFlow = () => {
   return (
     <div className="mt-20">
       <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide">
-        Accelerate your{' '}
-        <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
-          coding workflow.
+        Supercharge Your{' '}
+        <span className="bg-gradient-to-r from-blue-600 to-blue-800 text-transparent bg-clip-text">
+          Software Journey
         </span>
       </h2>
       <div className="flex flex-wrap justify-center">
@@ -18,7 +18,7 @@ const WorkFlow = () => {
         <div className="pt-12 w-full lg:w-1/2">
           {checklistItems.map((item, index) => (
             <div key={index} className="flex mb-12">
-              <div className="text-green-400 mx-6 bg-neutral-900 h-10 w-10 p-2 justify-center items-center rounded-full">
+              <div className="text-blue-400 mx-6 bg-neutral-900 h-10 w-10 p-2 justify-center items-center rounded-full">
                 <CheckCircle2 />
               </div>
               <div>
